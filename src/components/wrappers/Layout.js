bc3a600ef21499e49276328b5f602743c680e497
Abdullah Ar Raqib <raqib.taf@gmail.com>
2018-10-14 13:08:09 +0600

    fix: overlay opacity

--- src/components/wrappers/Layout.js
+++ src/components/wrappers/Layout.js
@@ -7,12 +7,14 @@ import theme from '../utility/theme'
 import Sidebar from '../Sidebar'
 
 const Overlay = styled.div`
-  position: absolute;
+  position: fixed;
   top: 0;
   right: 0;
   bottom: 0;
   left: 0;
-  background-color: rgba(19, 21, 27, 0.8);
+  z-index: 1;
+  opacity: 0.9;
+  background-color: ${theme.bodyBg};
   @media (min-width: ${media.md}) {
     display: none;
   }
@@ -77,6 +79,7 @@ const LayoutSidenav = styled(PosedLayoutSidenav)`
 `
 
 const LayoutContainer = styled(LayoutContainerBase)`
+  width: 100%;
   padding-left: 85px;
   display: flex;
   flex-direction: column;
